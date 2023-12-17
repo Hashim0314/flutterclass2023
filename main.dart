d00a035cee48fdc907d61b5f4223b904c6b35ec9
unknown <Hashim0314>
2023-12-17 18:27:10 +0500

    loop work

--- main.dart
+++ main.dart
@@ -1,7 +1,14 @@
+import 'dart:io';
 void main(){
-  print('hellow world');
-  print('hellow git hub');
-  print('hellow 3rd time');
-  print('4 time');
-  print("hello world 6");
+  // print('hellow world');
+  // print('hellow git hub');
+  // print('hellow 3rd time');
+  // print('4 time');
+  // print("hello world 6");
+ print('Enter Number for Generate Mathematical Table');
+ int numbr = int.parse(stdin.readLineSync()!);
+ //int numbr2 = int.parse(stdin.readLineSync()!);
+ for(var i = 1; i<=10; i++){
+   print('$numbr * ${i} = ${i * numbr}');
+   }
 }
